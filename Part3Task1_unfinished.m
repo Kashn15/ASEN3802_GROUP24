05b34bb5e4de839f78df4f71c864fadaa0dba8a1
alfe7630 <alfe7630@colorado.edu>
2026-03-25 09:13:06 -0600

    Update Part3Task1_unfinished.m

--- Part3Task1_unfinished.m
+++ Part3Task1_unfinished.m
@@ -1,15 +1,20 @@
+%% ASEN 3802 | LAB 02 Part 3
+% Author: Alec Fenwick
+% Editor: Natsumi Kakuda
+
 clear;
 clc;
 close all;
-    %% Part 1 Code Used for Part 3
+
+%% Part 1 Code Used for Part 3
 dataFolder = 'ASEN3802_HeatConduction_FA25';
 a = dir(fullfile(dataFolder, '*mA'));
 A = (pi/4) * (0.0254^2);
 for i = 1:length(a)
+
     % Parse filename
-    
     b = strsplit(a(i).name,'_');
-material = b{1};
+    material = b{1};
 
 if strcmp(material,'Aluminum')
     k = 130;
@@ -31,24 +36,26 @@ end
     % Delete first row
     data(1,:) = [];
     time = data(:,1);
-    Dat = data(:,2:9);   % CH1–CH8
+    Dat = data(:,2:9); % CH1–CH8
 
     % steady state
     N = length(time);
-    steady_index = round(0.9*N):N-2;   % last 10% of data assumed steady (we took the last 2 off which were NaN)
+    steady_index = round(0.9*N):N-2; % last 10% of data assumed steady (we took the last 2 off which were NaN)
 
     Dat_steady = mean(Dat(steady_index,:),1);
 
     % Thermocouple positions (meters)
-x1= 1+3/8;
-x2 = x1+0.5;
-x3 = x2+0.5;
-x4 = x3+0.5;
-x5 = x4+0.5;
-x6 = x5+0.5;
-x7 = x6+0.5;
-x8 = x7+0.5;
-    x = 0.0254.*[x1 x2 x3 x4 x5 x6 x7 x8]; %METERS
+x1 = 1+ (3/8);
+x2 = x1 + 0.5;
+x3 = x2 + 0.5;
+x4 = x3 + 0.5;
+x5 = x4 + 0.5;
+x6 = x5 + 0.5;
+x7 = x6 + 0.5;
+x8 = x7 + 0.5;
+x = 0.0254 .* [x1 x2 x3 x4 x5 x6 x7 x8]; %METERS
+
+L = 0.149225; % [m]
 
     % Linear fit for experimental
 
@@ -61,47 +68,24 @@ x8 = x7+0.5;
     H_an = power/(k*A);
 
     % Tables 
-T_0_table(i) = T_0;
-H_exp_table(i) = H_exp;
-H_an_table(i) = H_an;
-Table_Task1(i,:) = [T_0_table(i), H_exp_table(i), H_an_table(i)];
-    % Plot
-%     figure;
-%     plot(x, x.*H_an+T_0);
-%     hold on
-%     plot(x, polyval(p,x))
-%     errorbar(x,Dat_steady, 2*ones(size(Dat_steady)))
-%     xlabel('Position (m)')
-%     ylabel('Temperature (°C)')
-%     title([a(i).name ' Steady State'])
-%     grid on
-% hold off
+    T_0_table(i) = T_0;
+    H_exp_table(i) = H_exp;
+    H_an_table(i) = H_an;
+    Table_Task1(i,:) = [T_0_table(i), H_exp_table(i), H_an_table(i)];
 
 % Task 2
-initial_index = 1:round(0.1*N);   % first 10% of data
-T_init = mean(Dat(initial_index,:),1);
-
-p_init = polyfit(x, T_init, 1);
-M_exp = p_init(1);
-M_0 = polyval(p_init,0);
-
-% figure;
-% scatter(x, T_init, 50)
-% hold on
-% plot(x, polyval(p_init,x))
-% xlabel('Position (m)')
-% ylabel('Temperature (°C)')
-% title([a(i).name ' Initial Temperature Distribution'])
-% legend('Initial Data','Linear Fit')
-% grid on
+initial_index = 1;
+    T_init = mean(Dat(initial_index,:),1);
+    p_init = polyfit(x, T_init, 1);
+    M_exp = p_init(1);
+    M_0 = polyval(p_init,0);
 
     %% Tables
     Table_Task2(i) = M_exp;
     
+%% Material Properties 
 
-%% Material Properties
-
-L = x(end);  
+% L = x(end);  
 
 % thermal diffusivity per material
 if strcmp(material,'Aluminum')
@@ -117,13 +101,16 @@ elseif strcmp(material,'Steel')
     c_p=500;
     rho=8000;
 end
+
 alpha = k/(rho*c_p);
 
 %% Part 3 Task 1
 
 % number of modes
 modes = 20;   % Increased from 10 to 20 for better resolution on plots
-alpha_scale_values = linspace(0.3, 3, 25); % What range????? 0.6 to 1.4****
+%alpha_scale_values = linspace(0.3, 3, 25); % What range????? 0.6 to 1.4****
+alpha_scale_values = linspace(0.6, 1.4, 25); % What range????? 0.6 to 1.4****
+RMS_values = zeros(1,25);
 
 for s = 1:length(alpha_scale_values)
     T_test = zeros(length(time),length(x)); % important to delete previous material's data
@@ -148,7 +135,15 @@ for j = 1:length(x)
         T_test(t_i,j) = T_0 + H_exp*xpos - transient_sum;
      end   
 end
-RMS_values(s) = sqrt(mean((T_test(:) - Dat(:)).^2));
+
+time_start = 0;              
+time_end = 0.45 * max(time);  
+
+mask = (time >= time_start) & (time <= time_end);
+
+RMS_values(s) = sqrt(mean((T_test(mask,:) - Dat(mask,:)).^2, 'all', 'omitnan'));
+
+%RMS_values(s) = sqrt(mean((T_test(:) - Dat(:)).^2, 'omitnan'));
 end
 % Best alpha
 [~, idx] = min(RMS_values);
@@ -156,6 +151,15 @@ best_alpha = alpha_scale_values(idx) * alpha;
 
 alpha_original(i) = alpha;
 alpha_adjusted(i) = best_alpha;
+
+scale_min = min(alpha_scale_values);
+scale_max = max(alpha_scale_values);
+alpha_percent = -100 + (alpha_scale_values - scale_min) .* (200 / (scale_max - scale_min));
+
+%plot RMS
+figure;
+plot(alpha_percent, RMS_values);
+
 
 % New model with best alpha
 T_model = zeros(length(time),length(x));
@@ -181,18 +185,17 @@ for j = 1:length(x)
 end
 
 % Plots
-figure
-hold on
-
+figure();
+hold on;
 for j = 1:8  
-    plot(time,Dat(:,j),'LineWidth',1)      % exp
-    plot(time,T_model(:,j),'--','LineWidth',1.5) % an 
+    plot(time,Dat(:,j),'k-','LineWidth',1)      % exp
+    plot(time,T_model(:,j),'r--','LineWidth',1.5) % an 
 end
 xlabel('Time (s)')
 ylabel('Temperature (°C)')
+legend('Experimental Model','Model III', 'Location','eastoutside');
 title([material ' ' num2str(volts(i)) 'V - Model III'])
-legend('TH1:Experimental','TH1:Analytical Model', 'TH2 Exp', 'TH2 An', 'TH3 Exp', 'TH3 An', 'TH4 Exp', 'TH4 An', 'TH5 Exp', 'TH5 An', 'TH6 Exp', 'TH6 An', 'TH7 Exp', 'TH7 An', 'TH8 Exp', 'TH8 An','Location','westoutside')
 grid on
 end
 
-alpha_tbl = table(alpha_original', alpha_adjusted', 'VariableNames', {'Alpha_Original', 'Alpha_Adjusted'});
+alpha_tbl = table(alpha_original, alpha_adjusted, 'VariableNames', {'Alpha_Original', 'Alpha_Adjusted'});
